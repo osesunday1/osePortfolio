2146ac9139c7e5f500b6a2c5d1849f49ce6c76c8
ose <ose.sunday@gmail.com>
2023-09-23 00:46:13 +0100

    up

--- Ose_project/settings.py
+++ Ose_project/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 '''
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -117,8 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_URL = 'static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build' 'static')
+STATICFILES_DIRS = [STATIC_DIR, ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- Ose_project/urls.py
+++ Ose_project/urls.py
@@ -7,5 +7,3 @@ from django.conf import settings
 urlpatterns = [
     path('',  views.home, name = 'home'),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
