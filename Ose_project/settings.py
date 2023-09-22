ead8d21bcf1d17c02b6b4ede2b325f6fb8e0a5b2
ose <ose.sunday@gmail.com>
2023-09-23 00:36:42 +0100

    sf

--- Ose_project/settings.py
+++ Ose_project/settings.py
@@ -117,10 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build' 'static')
-STATICFILES_DIRS = [STATIC_DIR, ]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
