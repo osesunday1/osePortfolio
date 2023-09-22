365b29092272ed1d6dfa443d7bf54285c12a9dd0
ose <ose.sunday@gmail.com>
2023-09-23 00:35:32 +0100

    assa

--- Ose_project/settings.py
+++ Ose_project/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'Ose_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -82,7 +82,7 @@ DATABASES = {
     }
 }
 
-
+'''
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
